239bb79a33896565d82c7538951107fa7cd11cf1
Sofie Verrewaere <sofieverrewaere@SofieVerrewaere-MacBook-Pro.local>
2022-08-07 18:56:16 +0100

    ensure folders are created and gif is generated

--- src/test.py
+++ src/test.py
@@ -1,6 +1,10 @@
+import imageio
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+
+from os import walk
 from keras.models import model_from_json
 
 from env import Catch
@@ -8,10 +12,19 @@ from env import Catch
 # Make sure this grid size matches the value used fro training
 grid_size = 10
 
+# Ensure folders are created
+model_path = "./model/"
+image_path = "./image/"
+gif_path = "./gif/"
+
+os.makedirs(image_path, exist_ok=True)
+os.makedirs(gif_path, exist_ok=True)
+
+
 # Load the trained model
-with open("model.json", "r") as jfile:
+with open(model_path + "model.json", "r") as jfile:
     model = model_from_json(json.load(jfile))
-model.load_weights("model.h5")
+model.load_weights(model_path + "model.h5")
 model.compile("sgd", "mse")
 
 # Define environment, game
@@ -20,16 +33,15 @@ c = 0
 
 # Run n iterations of the game
 for e in range(10):
-    loss = 0.
+    loss = 0.0
     env.reset()
     game_over = False
     # get initial input
     input_t = env.observe()
 
     # Draw the environment
-    plt.imshow(input_t.reshape((grid_size,)*2),
-                interpolation='none', cmap='gray')
-    plt.savefig("%03d.png" % c)
+    plt.imshow(input_t.reshape((grid_size,) * 2), interpolation="none", cmap="gray")
+    plt.savefig(image_path + "%03d.png" % c)
 
     c += 1
 
@@ -43,7 +55,15 @@ for e in range(10):
         # apply action, get rewards and new state
         input_t, reward, game_over = env.act(action)
 
-        plt.imshow(input_t.reshape((grid_size,)*2),
-                    interpolation='none', cmap='gray')
-        plt.savefig("%03d.png" % c)
+        plt.imshow(input_t.reshape((grid_size,) * 2), interpolation="none", cmap="gray")
+        plt.savefig(image_path + "%03d.png" % c)
         c += 1
+
+# Generate Gif
+
+
+filenames = next(walk(image_path), (None, None, []))[2]
+images = []
+for filename in filenames:
+    images.append(imageio.imread(image_path + filename))
+imageio.mimsave(gif_path + "catch.gif", images)
